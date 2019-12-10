3aaf21784250c9f37f152c29466e2267b3eb3f42
Anwesh43 <anweshthecool0@gmail.com>
2019-12-11 03:37:13 +0530

    adding view in activity and fullscreen

--- app/src/main/java/com/anwesh/uiprojects/linkedbouncyrightangleview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbouncyrightangleview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedbouncyrightangleview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.bouncyrightangleview.BouncyRightAngleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BouncyRightAngleView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
