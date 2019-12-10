b55167481dfc8d67741df8f944cfc8adbaa84c91
Anwesh43 <anweshthecool0@gmail.com>
2019-12-11 03:12:55 +0530

    adding constants and libraries

--- bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
+++ bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.bouncyrightangleview
 /**
  * Created by anweshmishra on 11/12/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
