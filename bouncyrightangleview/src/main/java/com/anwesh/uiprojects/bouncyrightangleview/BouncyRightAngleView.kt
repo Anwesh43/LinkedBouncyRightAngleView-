56a13f63130c19baa20afb04f89bebe865fd470f
Anwesh43 <anweshthecool0@gmail.com>
2019-12-11 03:29:45 +0530

    adding BouncyRightAngle class

--- bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
+++ bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
@@ -158,4 +158,29 @@ class BouncyRightAngleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+
+    data class BoucnyRightAngle(var i : Int) {
+
+        private val root : BRANode = BRANode(0)
+        private var curr : BRANode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
