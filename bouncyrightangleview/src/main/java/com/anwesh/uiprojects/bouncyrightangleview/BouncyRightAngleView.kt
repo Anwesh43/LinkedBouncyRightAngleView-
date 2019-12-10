6b39edecfde87a7f47dcad9d3d92e870a5e84b56
Anwesh43 <anweshthecool0@gmail.com>
2019-12-11 03:21:26 +0530

    adding BouncyRightAngleView class

--- bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
+++ bouncyrightangleview/src/main/java/com/anwesh/uiprojects/bouncyrightangleview/BouncyRightAngleView.kt
@@ -51,3 +51,21 @@ fun Canvas.drawBRANode(i : Int, scale : Float, paint : Paint) {
     drawBouncyRightAngle(scale, size, h, paint)
     restore()
 }
+
+class BouncyRightAngleView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN  -> {
+
+            }
+        }
+        return true
+    }
+}
